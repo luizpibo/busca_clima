71667fbfa4260e225dc5537a62edb2cff36f00be
Luiz Fernando <luizfernandolima_09@hotmail.com>
2022-01-13 23:08:13 -0300

    detalhes

--- src/componentes/Container/index.js
+++ src/componentes/Container/index.js
@@ -3,9 +3,10 @@ import Styled from "styled-components";
 const Container = Styled.div`
     align-items: center;
     display: flex;
-    height: 100vh;
+    min-height: 100vh;
     justify-content: center;
     width: 100%;
+    margin-bottom: 2rem;
 `;
 
 export default Container;
--- src/componentes/Wrapper/index.js
+++ src/componentes/Wrapper/index.js
@@ -4,6 +4,7 @@ const Wrapper = Styled.div`
     background-color:  #FB8B16;
     border-radius: 1rem;
     padding: 1rem;
+    height: fit-content;
     width: 80%;
 `;
 
